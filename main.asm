47dd213d17fdfe70c7e2f2d7a589ccd06e994166
Anna Ernst <49048470+annaernst@users.noreply.github.com>
2022-04-28 18:08:10 -0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -6,7 +6,6 @@ frameBuffer:	.align 2
 	colorP2:	.word 0x00ffff00
 	colorDarker:	.word 0x00999999
 	cColmSelect:	.word 1
-
 board:      
 		.word 0, 0, 0, 0, 0, 0, 0
 		.word 0, 0, 0, 0, 0, 0, 0
@@ -28,7 +27,7 @@ userwinprompt: .asciiz "You have won!\n"
 compwinprompt: .asciiz "The Computer has won!\n"
 
 
-
+.globl board inputError drawPlayerPiece
 
 .text
 	jal drawGameOnBoot
@@ -36,7 +35,7 @@ compwinprompt: .asciiz "The Computer has won!\n"
 
      
 inputLoop:
-	jal checkForWin
+	#jal checkForWin
 	
 
 	
@@ -44,69 +43,133 @@ inputLoop:
 	li $v0, 12            
      	syscall
      
-     	add	$v0, $zero, 10
-	syscall
-#     	beq $a0, 'a', colLeft		# select column left
-#     	beq $a0, 'd', colRight		# select column right
-#     	beq $a0, 's', makeAMove	# play in current column
-#     	j inputLoop
+     	#add	$v0, $zero, 10
+	#syscall
+   	beq $v0, 'a', colLeft		# select column left
+     	beq $v0, 'd', colRight		# select column right
+     	beq $v0, 's', makeAMove	# play in current column
+     	j inputLoop
               
 makeAMove:
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
 # Check for invalid input (column full?)
-#     	la  $a0, board				# base address of board into $a0
-#     	lw  $a1, COL_SIZE			# num of coloumns into 	     $a1
-#     	lw $a3, colIndex	
-#     	jal validInput	   
+    	la  $a0, board				# base address of board into $a0
+     	lw  $a1, COL_SIZE			# num of coloumns into 	     $a1
+     	lw $a3, colIndex	
+     	jal validInput
 # Upload to Array
-#     	la $a0, board
-#     	addi $a2, $0, -1			# row index set to -1
-#     	lw  $a1, COL_SIZE			# num of coloumns into 	     $a1
-#     	jal addValUser				# add to board(next available row)	
+     	la $a0, board
+     	addi $a2, $0, -1			# row index set to -1
+     	lw  $a1, COL_SIZE			# num of coloumns into 	     $a1
+ #    	jal addValUser				# add to board(next available row)	
 #	jal computerValid
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	j startingMoves
 
-addValUser:	#$a0 -> base addr, $a1 -> COL_SIZE,  $a2 -> row index,  $a3 -> coloumn index, 	
-#	addi $a2, $a2, 1
-#	move $s0, $ra
+#addValUser:	#$a0 -> base addr, $a1 -> COL_SIZE,  $a2 -> row index,  $a3 -> coloumn index, 	
+#	addi $a2, $zero, 1
+#	addi $sp, $sp, -4
+#	sw $ra, 0($sp)
 #	jal getAt		#result in $v0, address in $v1
-#	bne $v0, $0, addValUser
-#	move $ra, $s0
-#	# set arguments before call
-#	addi $t0, $0, 1    
+#	lw $ra, 0($sp)
+#	addi $sp, $sp, 4
+	#beqz $v0, addValUser
+	# set arguments before call
+#	addi $t0, $zero, 1    
 #	sw $t0, ($v1)
 #	jr $ra
 
 # Check Valid Input
 validInput:
 # Check if coloumn is full		(getAt function) $a0 -> base addr, $a1 -> COL_SIZE,  $a2 -> row index,  $a3 -> coloumn index
-#    	addi $a2, $0, 5			# row index into $a2
-#    	move $s0, $ra
-#     	jal getAt
-#     	# result in $v0
-#     	move $ra, $s0
-#     	bnez $v0, inputError		# if value is not ZERO (empty), then retake input
-#     	jr $ra				# else continue program
+    	addi $a2, $zero, 5			# row index into $a2
+    	addi $sp, $sp -4
+    	sw $ra, 0($sp)
+     	jal getAt
+     	# result in $v0
+     	#bnez $v0, inputError		# if value is not ZERO (empty), then retake input
+     	
+     	li $t9, 1
+     	li $t7, 7
+     	
+     	mul $t7, $t7, $a2
+     	add $t8, $t7, $a3
+     	sll $t8, $t8, 2
+     	sw $t9, board($t8)
+     	
+     	addi $sp, $sp, -20
+     	sw $ra, 0($sp)
+     	sw $a0, 4($sp)
+     	sw $a1, 8($sp)
+     	sw $a2, 12($sp)
+     	sw $a3, 16($sp)
+     	add $a0, $a3, $zero
+     	add $a1, $a2, $zero
+     	addi $a2, $zero, 1
+     	
+     	jal drawPlayerPiece
+     	
+     	lw $ra, 0($sp)
+     	lw $a0, 4($sp)
+     	lw $a1, 8($sp)
+     	lw $a2, 12($sp)
+     	lw $a3, 16($sp)
+     	addi $sp, $sp, 20
+     	
+     	lw $ra, 0($sp)
+     	addi $sp, $sp, 4
+     	jr $ra				# else continue program
      
     
 getAt:	#$a0 -> base addr, $a1 -> COL_SIZE,  $a2 -> row index,  $a3 -> coloumn Index
-#	move $s1, $t0
-#	add $t0, $t0, $0
-#     	lw  $a3, colIndex
-#     	mul $t0, $a1, $a2 		        # row index * COL_SIZE
-#     	add $t0, $t0, $a3			# + coloumnIndex
-#     	mul $t0, $t0, DATA_SIZE		# * Data Size
-#     	add $t0, $t0, $a0			# + base addr
-#     	lw  $v0, ($t0)				# value in $v0
-#     	la  $v1, ($t0)
-#	move $t0, $s1
-#     	jr  $ra				
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	#move $s1, $t0 
+     	#lw  $a3, colIndex
+     	#mul $t0, $a1, $a2 		        # row index * COL_SIZE
+     	#add $t0, $t0, $a3			# + coloumnIndex
+     	#mul $t0, $t0, DATA_SIZE		# * Data Size
+     	#add $t0, $t0, $a0			# + base addr
+     	#lw  $v0, 0($t0)				# value in $v0
+     	#la  $v1, ($t0)
+	#move $t0, $s1
+	
+	
+     	
+     	
+     	getAtLoop:
+     	li $t7, 7
+     	bltz $a2, inputError
+     	mul $t7, $t7, $a2
+     	add $t8, $t7, $a3
+     	sll $t8, $t8, 2
+     	lw $t9, board($t8)
+     	
+     	beqz $t9, getAtLoopEnd
+     	addi $a2, $a2, -1
+     	j getAtLoop
+     	#check if t9 is 0
+     	#if it is zero, we can continue and place the piece
+     	#if it is not zero, we increment $a2 by 1 and repeat the above section
+     	#except, if after the increment $a2 is greater than 6, in which we send an error
+	
+	getAtLoopEnd:
+	
+	
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+     	jr  $ra				
 
      
 inputError:
-#	jal errorSound
-#     li $v0, 4
-#     la $a0, printInE
-#     syscall
-#     j inputLoop
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	jal errorSound
+	lw $ra, 0($sp)
+	addi $sp, $sp 4
+	j inputLoop
      
 #------------------
 # COLUMN SELECTION
@@ -114,93 +177,90 @@ inputError:
 
 # decrement colIndex
 colLeft:
-#	lw $t9, colIndex
-#	li $t8, 0
-#	beq $t9, $t8, colLast 		# if first column, select last
-#	li $t8, 1
-#	sub $t9, $t9, $t8		# otherwise, move one left
-#	sw $t9, colIndex
-#	lw $a0, colIndex
-#	jal colorColmSelect
-#	j inputLoop
+	lw $t9, colIndex
+	beq $t9, $zero, colLast 		# if first column, select last
+	addi $t9, $t9, -1		# otherwise, move one left
+	sw $t9, colIndex
+	lw $a0, colIndex
+	jal colorColmSelect
+	j inputLoop
 # increment colIndex
 colRight:
-#	lw $t9, colIndex
-#	lw $t8, COL_SIZE
-#	sub $t8, $t8, 1			# zero index adjustment
-#	beq $t9, $t8, colFirst		# if last column, select first
-#	li $t8, 0
-#	add $t9, $t9, $t8		# otherwise, move one right
-#	sw $t9, colIndex
-#	lw $a0, colIndex
-#	jal colorColmSelect
-#	j inputLoop
+	lw $t9, colIndex
+	lw $t8, COL_SIZE
+	sub $t8, $t8, 1			# zero index adjustment
+	beq $t9, $t8, colFirst		# if last column, select first
+	addi $t9, $t9, 1		# otherwise, move one right
+	sw $t9, colIndex
+	lw $a0, colIndex
+	jal colorColmSelect
+	j inputLoop
 # set colIndex to the last column
 colLast:
-#	lw $t9, COL_SIZE
-#	sub $t9, $t9, 1			# zero index adjustment
-#	sw $t9, colIndex
-#	lw $a0, colIndex
-#	jal colorColmSelect
-#	j inputLoop
+	lw $t9, COL_SIZE
+	sub $t9, $t9, 1			# zero index adjustment
+	sw $t9, colIndex
+	lw $a0, colIndex
+	jal colorColmSelect
+	j inputLoop
 # set colIndex to 0
 colFirst:
-#	li $t9, 0
-#	sw $t9, colIndex
-#	lw $a0, colIndex
-#	jal colorColmSelect
-#	j inputLoop
+	li $t9, 0
+	sw $t9, colIndex
+	lw $a0, colIndex
+	jal colorColmSelect
+	j inputLoop
     
 computerValid:
-#   addi $sp, $sp, -4   
-#   sw $ra, 0($sp)     
+   addi $sp, $sp, -4   
+   sw $ra, 0($sp)     
         
-#    li $v0, 42  
-#    syscall
-#    addi $a1, $a0, 50
-#    li $v0, 42  
-#    syscall
-#   subu $t0, $a1, $a0  
-#   addiu $t0, $t0, 1  
+    li $v0, 42  
+    syscall
+    addi $a1, $a0, 50
+    li $v0, 42  
+    syscall
+   subu $t0, $a1, $a0  
+   addiu $t0, $t0, 1  
 
    
    # Compute Computer's position and return
-#   divu $t1, $t0, 7      
-#   mfhi $t2            #t2 is the mod with 7 for column
-#   move $v0, $t2       
-#   jr $ra              
+   divu $t1, $t0, 7      
+   mfhi $t2            #t2 is the mod with 7 for column
+   move $v0, $t2       
+   jr $ra              
         
         
  computerMove:
-#        li $a0, 0                         
-#        li $a1, 6                
-#        addi $sp, $sp, -4                 
-#        sw $ra, 0($sp)                  
+        li $a0, 0                         
+        li $a1, 6                
+        addi $sp, $sp, -4                 
+        sw $ra, 0($sp)                  
    
         computer: 
-#        	  jal computerValid    
-#                  move $t2, $v0            #moves the random number of $v0 to t2, which will be the column computer will choose
-#                  addi $t5, $t2, -1        #decreases the value by 1 in register t5
-#                  blt $t2, $a0, computer #loop back if number is less than 1
-#                  bgt $t2, $a1, computer   #loop back if number is greater than 7
-#                  li $t6, 4                
-#                  mul $t4, $t5, $t6       #$t4 = i(computer column - 1) * 4 (index times 4 bytes)
-#                  add $t4, $s5, $t4        # $t4 = placement of computer Column - 1 (Base + Offset)
-#                  lw $t7, 0($t4)           
+        	  jal computerValid    
+                  move $t2, $v0            #moves the random number of $v0 to t2, which will be the column computer will choose
+                  addi $t5, $t2, -1        #decreases the value by 1 in register t5
+                  blt $t2, $a0, computer #loop back if number is less than 1
+                  bgt $t2, $a1, computer   #loop back if number is greater than 7
+                  li $t6, 4                
+                  mul $t4, $t5, $t6       #$t4 = i(computer column - 1) * 4 (index times 4 bytes)
+                  add $t4, $s5, $t4        # $t4 = placement of computer Column - 1 (Base + Offset)
+                  lw $t7, 0($t4)           
                   
-#                  blt $t7, $zero, computer # if placement value less than 0, Branch to computer again
+                  blt $t7, $zero, computer # if placement value less than 0, Branch to computer again
          
        #used to set the next row for the computer placement 
-#        mul $t3, $t7, $s3                  
-#        add $t3, $t3, $t2                
-#        add $t3, $s4, $t3              
-        #instruction to place $t3 into the graphics                    
+        mul $t3, $t7, $s3                  
+        add $t3, $t3, $t2                
+        add $t3, $s4, $t3              
+       #instruction to place $t3 into the graphics                    
           
         # Decrement by 1 for next placement
-#        addi $t7, $t7, -1                  
-#        sw $t7, 0($t4)                   
-#        lw $ra, 0($sp)                   
-#        addi $sp, $sp, 4                 
+        addi $t7, $t7, -1                  
+        sw $t7, 0($t4)                   
+        lw $ra, 0($sp)                   
+        addi $sp, $sp, 4                 
 
 
 #---------------
@@ -208,12 +268,12 @@ computerValid:
 #---------------
 
 errorSound:
-#	li $a0, 72 # pitch (0-127) - this is the C an octave above middle C
-#	li $a1, 1000 # duration of each sound in milliseconds (1000 = 1 second)
-#	li $a3, 100 # volume (0-127)
-#	li $a2, 55
-#	li $v0, 33
-#	jr $ra
+	li $a0, 72 # pitch (0-127) - this is the C an octave above middle C
+	li $a1, 1000 # duration of each sound in milliseconds (1000 = 1 second)
+	li $a3, 100 # volume (0-127)
+	li $a2, 55
+	li $v0, 33
+	jr $ra
 
 dropSound:
 #	li $a0, 67 # pitch (0-127) 
@@ -430,6 +490,8 @@ colmEraseLoop:
 	
 	
 	move	$a0, $s2
+	addi	$s2, $s2, 1
+	sw	$s2, cColmSelect($zero)
 	add	$a0, $a0, 1
 	mul	$a0, $a0, 63
 	add	$a0, $a0, -12
@@ -1547,6 +1609,3 @@ drawPoint:				# fills point at x = $a0, y = $a1, with color $a3
 	addi	$sp, $sp, 4
 	jr	$ra
 	
-	
-	
-
