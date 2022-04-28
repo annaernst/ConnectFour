a3bc764eb7e56fbdd6de7ec28a8bd4a09ddb0fef
Vijayyk1 <71900137+Vijayyk1@users.noreply.github.com>
2022-04-28 15:49:19 -0500

    Update computerMove.asm

--- computerMove.asm
+++ computerMove.asm
@@ -1,41 +1,81 @@
+  .data
+    increment: .word 1
+      
+   .text
     #Starting moves begin in center and tries to control as much of center as possible
-    startingMoves:
-    	li $a1, 3
-    	addi $a0, $zero, 0 
-    	li $a2, 2
-    	jal drawPlayerPiece
-    	li $a1, 3
-    	addi $a0, $zero, 1
-    	li $a2, 2
-    	jal drawPlayerPiece
-    	li $a1, 4
-    	addi $a0, $zero, 0 
-    	li $a2, 2
-    	jal drawPlayerPiece
-    	li $a1, 1
-    	addi $a0, $zero, 0 
-    	li $a2, 2
-    	jal drawPlayerPiece
-    	li $a1, 2
-    	addi $a0, $zero, 0 
-    	li $a2, 2
-    	jal drawPlayerPiece
-    	addi $a0, $zero, 0 
-    computerValid:
-   addi $sp, $sp, -4   
-   sw $ra, 0($sp)     
+   startingMoves:
+   addi $sp, $sp, -8   
+   sw $ra, 0($sp) 
+   sw $s0, 4($sp)  
+     
+        lw $t8, increment #increment for moves
+        startingPoint:
+        li $t3, 1
+        beq $s0, $t3, move1
+        li $t3, 2
+        beq $s0, $t3, move2
+        li $t3, 3
+        beq $s0, $t3, move3
+        li $t3, 4
+        beq $s0, $t3, move4
+        li $t3, 5
+        beq $s0, $t3, move5
+        jal computerMove
+    cpuTurnEnd:
         
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
+    jr $ra
+       
+       move1:
+    	li $a0, 3
+    	addi $a1, $zero, 0 
+    	li $a2, 2
+    	jal drawPlayerPiece
+    	addi $s0, $s0, 1
+    	sw $s0, increment($zero)
+    	j cpuTurnEnd
+       move2: 
+    	li $a0, 3
+    	addi $a1, $zero, 1
+    	li $a2, 2
+    	jal drawPlayerPiece
+    	addi $s0, $s0, 1
+    	sw $s0, increment($zero)
+    	j cpuTurnEnd
+       move3:
+    	li $a0, 4
+    	addi $a1, $zero, 0 
+    	li $a2, 2
+    	jal drawPlayerPiece
+    	addi $s0, $s0, 1
+    	sw $s0, increment($zero)
+    	j cpuTurnEnd
+      move4:
+    	li $a0, 1
+    	addi $a1, $zero, 0 
+    	li $a2, 2
+    	jal drawPlayerPiece
+    	addi $s0, $s0, 1
+    	sw $s0, increment($zero)
+    	j cpuTurnEnd
+    	move5:
+    	li $a0, 2
+    	addi $a1, $zero, 0 
+    	li $a2, 2
+    	jal drawPlayerPiece
+    	addi $s0, $s0, 1
+    	sw $s0, increment($zero)
+    	j cpuTurnEnd
+    	
+    	
+   lw $ra, 0($sp)
+   addi $sp, $sp, 4
+   jr $ra
+    computerValid:
     li $v0, 42  
     syscall
-    addi $a1, $a0, 50
-    li $v0, 42  
-    syscall
-   subu $t0, $a1, $a0  
-   addiu $t0, $t0, 1  
-   
-   # li $v0, 42  
-   # syscall
-   #move $t0, $a0
+    move $t0, $a1
    
    # Compute Computer's position and return
    divu $t1, $t0, 7      
@@ -51,29 +91,21 @@
         computer: 
         	  jal computerValid    
                   move $t2, $v0            #moves the random number of $v0 to t2, which will be the column computer will choose
-                  addi $t5, $t2, -1        #decreases the value by 1 in register t5
                   blt $t2, $a0, computer #loop back if number is less than 0
-                  bgt $t2, $a1, computer   #loop back if number is greater than 6
-                  li $t6, 4                
-                  mul $t4, $t5, $t6       
-                  add $t4, $s5, $t4        
-                  lw $t7, 0($t4)           
+                  bgt $t2, $a1, computer   #loop back if number is greater than 6           
                   blt $t7, $zero, computer # if placement value less than 0, Branch to computer again
          
-       #used to set the next row for the computer placement 
-        mul $t3, $t7, $s3                  
-        add $t3, $t3, $t2                
-        add $t3, $s4, $t3   
-        move $a0, $t5            
-        move $a1, $t3           #a1
+        move $a1, $t5            #y
+        move $a0, $t2           #x
         li $a2, 2
         jal drawPlayerPiece
         # decrement by 1 for next placement
-        addi $t7, $t7, -1                  
-        sw $t7, 0($t4)                   
-        lw $ra, 0($sp)                   
+        addi $a1, $a1, 1                 
+                           
+        lw $ra, 0($sp)     #restores from stack
         addi $sp, $sp, 4                 
         
         #restores registers from stack
         lw $ra, 0($sp)                     
         addi $sp, $sp, 4 
+        
